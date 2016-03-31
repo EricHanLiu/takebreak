ea6d2d273c91f87754a11308154c49d23653c559
Eric Han Liu <ericliu404@gmail.com>
2016-03-30 22:31:05 -0400

    Use os instead of subprocess

--- takebreak.py
+++ takebreak.py
@@ -1,11 +1,12 @@
-import subprocess
+import os
 import sys
+
 
 default = "Take a 20 second break - look at least 20 feet away!"
 message = sys.argv[1] if len(sys.argv) > 1 else default
 
 def sendmessage(message):
-    subprocess.Popen(['notify-send', message])
+    os.system('notify-send "Take a break!" "'+message+'"')
     return
 
 sendmessage(message)
