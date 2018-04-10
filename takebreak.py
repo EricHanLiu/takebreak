8ff905a5b9682540308034ea63579b25ffae9188
erichanliu <ericliu404@gmail.com>
2018-04-10 15:26:33 -0400

    Set message to default always

--- takebreak.py
+++ takebreak.py
@@ -2,8 +2,7 @@ import os
 import sys
 
 
-default = "Take a 20 second break - look at least 20 feet away!"
-message = sys.argv[1] if len(sys.argv) > 1 else default
+message = "Take a 20 second break - look at least 20 feet away!"
 
 def sendmessage(message):
     os.system('notify-send "Take a break!" "'+message+'"')
